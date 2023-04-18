03cdf7e31d4851ab4bebf44e433bef8e35821958
AgnidDrage <marianosancheztoledo@gmail.com>
2023-04-18 15:29:11 -0300

    Celery working fine and video process almost done

--- flowy_backend/flowy_backend.py
+++ flowy_backend/flowy_backend.py
@@ -7,6 +7,7 @@ import cv2
 import numpy as np
 from dotenv import load_dotenv
 from celery import Celery
+from concurrent.futures import ThreadPoolExecutor
 
 load_dotenv()
 BROKER_URL = os.getenv('BROKER_URL')
@@ -22,21 +23,79 @@ celery_app.conf.update(
 
 @celery_app.task
 def process_video(video_pickle):
-    print("llego a celery el video")
+    print("Video Received")
+    # Read pickle and save video name and the video itself in a temporal file.
+    video_name, video_bytes = pickle.loads(video_pickle)
+    with open(video_name, 'wb') as f:
+        f.write(video_bytes)
+    del video_pickle, video_bytes
 
-def prepare_video(video_name):
-    cap = cv2.VideoCapture(video_name) # abre el archivo de video
-    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) # obtiene el número de frames
-    frames = [] # crea una lista vacía para guardar los frames
+    # Prepare video for process converting it to a np.array
+    cap = cv2.VideoCapture(video_name) # Open video file
+    frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) # Read number of frames
+    framerate = int(cap.get(cv2.CAP_PROP_FPS)) # Read framerate
+    print(frame_count)
+    frames = [] # List to store frames
 
-    ret = True # inicializa una variable booleana
-    while ret: # mientras haya frames por leer
-        ret, img = cap.read() # lee un frame del objeto cap; img es un np.array de dimensiones (x, y, c)
-        if ret: # si se leyó correctamente el frame
-            frames.append(img) # lo agrega a la lista de frames
+    ret = True # Boolean flag 
+    while ret: # while there are frames to read
+        ret, img = cap.read() # read frame
+        if ret: # if the frame was read correctly
+            frames.append(img) # it is added to the list
 
-    video = np.stack(frames, axis=0) # convierte la lista de frames en un np.array de dimensiones (frame, x, y, c)
-    return video
+    video = np.stack(frames, axis=0) # convert the frame list into a ndarray with shape = (frame, x, y, channel)
+    print(video.shape)
+    os.remove(video_name)
+
+    # Process the video
+
+    # target_framerate = video.shape[0] * 2
+    # new_axis = np.linspace(0, video.shape[0]-1, target_framerate)
+
+    # # Interpolate the frame axis
+    # for i in range(video.shape[1]):
+    #     for j in range(video.shape[2]):
+    #         for k in range(video.shape[3]):
+    #             video[:, i, j, k] = np.interp(new_axis, np.arange(video.shape[0]), video[:, i, j, k])
+
+    # return video.shape
+    
+    # Divide the video in 3 chunks
+    chunks = np.array_split(video, 3, axis=0)
+
+    # Process each chunk in a different process
+    print("Generating threads")
+    with ThreadPoolExecutor(max_workers=3) as executor:
+        results = executor.map(process_chunk, chunks)
+    
+    print("Rebuilding video")
+    final_video = np.concatenate(list(results), axis=0)
+
+    # Normalize the video
+    final_video = (final_video / 255).astype(np.uint8)
+
+    # Save the video
+    print("Saving video")
+    out = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc(*'mp4v'), framerate*2, (final_video.shape[2], final_video.shape[1]))
+
+    for i in range(final_video.shape[0]):
+        out.write(final_video[i])
+
+    out.release()
+
+
+
+def process_chunk(chunk):
+    target_frames = chunk.shape[0] * 2
+    new_frame_axis = np.linspace(0, chunk.shape[0]-1, target_frames)
+    new_chunk = np.empty((target_frames, chunk.shape[1], chunk.shape[2], chunk.shape[3]))
+
+    # Interpolate the frame axis
+    for i in range(chunk.shape[1]):
+        for j in range(chunk.shape[2]):
+            for k in range(chunk.shape[3]):
+                new_chunk[:, i, j, k] = np.interp(new_frame_axis, np.arange(chunk.shape[0]), chunk[:, i, j, k])
+    return new_chunk
 
 class FlowyBackend(socketserver.ThreadingMixIn, socketserver.BaseRequestHandler):
     def handle(self):
